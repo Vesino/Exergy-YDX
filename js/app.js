ea1b1cb64607a58b1954a3b042cd35c78b2fc4f8
Luis Vargas <luisvargasr10@gmail.com>
2020-11-02 14:21:09 -0600

    Team section added

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const sendOrderButton = document.querySelector('#sendOrder')
+
+sendOrderButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    alert("We are currently working on this please accept our appologies")
+})
