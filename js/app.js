9a3c36a817f8420b81e67292f7bf9be05c21e609
Luis Vargas <luisvargasr10@gmail.com>
2020-11-02 18:34:31 -0600

    Fixing some details

--- js/app.js
+++ js/app.js
@@ -2,5 +2,5 @@ const sendOrderButton = document.querySelector('#sendOrder')
 
 sendOrderButton.addEventListener('click', (e) => {
     e.preventDefault()
-    alert("We are currently working on this please accept our appologies")
+    alert("We are currently working on this form, please accept our appologies")
 })
